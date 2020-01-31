b3367d0c90b3f89133fedd4f66006e8cc0a8cc5f
DanielGuramulta <Daniel.Guramulta@analog.com>
2020-02-01 00:03:58 +0200

    Send nice song to listen in case ci return build successful
    Signed-off-by: DanielGuramulta <Daniel.Guramulta@analog.com>

--- ci/travis/post_manele.sh
+++ ci/travis/post_manele.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-   
-   curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-                        -d "{\"body\": \"Hello world\"}" \
-                        "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+
+    message=($(curl -G https://www.googleapis.com/youtube/v3/search -d part="snippet" \
+      -d q="manelevesele" -d key="${YOUTUBE_DATA_TOKEN}" \
+      -d maxResults="50" -d type="video" | grep videoId | rev | cut -d: -f1 | rev | tr -d '"'))
+
+    message=${message[RANDOM%${#message[@]}]}
+    message="https://www.youtube.com/watch?v=${message}"
+
+    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+                        -d "{\"body\": \"Travis build ran successfully, celebrate by listening to: ${message} \"}" \
+                        "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments" &>/dev/null
 fi
